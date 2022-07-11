64ced5ada457af697b9b1de325d67a0cdb18f150
Gui Escobar <guieslopes99@hotmail.com>
2022-07-11 00:01:37 -0300

    grammar update

--- parse.js
+++ parse.js
@@ -3,6 +3,11 @@ const grammar = require("./grammar.js");
 
 const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar))
 
-parser.feed("foo\n")
+try{
+    parser.feed("2")
+    console.log("parse success", parser.results)
+} catch (e) {
+    console.log("parse error", e.message)
+}
 
 console.log(JSON.stringify(parser.results))
