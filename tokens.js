ba9bfc7663dfbf6459b0273c692798dab96a801d
Gui Escobar <guieslopes99@hotmail.com>
2022-07-14 20:55:27 -0300

    using moo tokens

--- tokens.js
+++ tokens.js
@@ -3,11 +3,12 @@ module.exports = {
     boolean: ['true', 'false'],
     keyword: ['if', 'then', 'else'],
     identifier: /[a-zA-Z_][a-zA-Z0-9_]*/,
-    number: /[0-9]+/,
+    number: /[+-]?([0-9]*[.])?[0-9]+/,
     string: /"(?:[^"\\]|\\.)*"/,
     comment: /\/\/.*/,
     punctuation: /[{}()\[\]]/,
     operator: /[+\-*/%<>=&|!]/,
+    cNull: 'null',
     NL: { match: /\n/, lineBreaks: true },
     eof: 'eof'
 }
