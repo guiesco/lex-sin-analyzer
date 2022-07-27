a62ee96edb97a1aec1827d5847a4087338c37b9e
Gui Escobar <guieslopes99@hotmail.com>
2022-07-26 23:11:00 -0300

    evolutions

--- tokens.js
+++ tokens.js
@@ -2,13 +2,15 @@
 const moo = require("moo")
 
 const mooLex = moo.compile({
-    whitespace: /[ \t]/,
-    boolean: ['true', 'false'],
     def: "def",
     keyword: ['if', 'else', 'return', 'read', 'print', 'for', 'new', 'def', 'break'],
     type_keyword: ['int', 'bool', 'float', 'string'],
+    boolean: ['true', 'false'],
     identifier: /[a-zA-Z_][a-zA-Z0-9_]*/,
     compare_keyword: ['<', '>', '<=', '>=', '!=', '=='],
+    string: /"(?:[^"\\]|\\.)*"/,
+    float: /(?:[0-9]*)?[.][0-9]+/,
+    int: /[0-9]+/,
     lparan: '(',
     rparan: ')',
     times: '*',
@@ -23,14 +25,8 @@ const mooLex = moo.compile({
     lbracket: '[',
     rbracket: ']',
     percentage: '%',
-    digit: /[0-9]/,
-    number: /(?:(?:[+-])?[0-9]*[.])?[0-9]+/,
-    string: /"(?:[^"\\]|\\.)*"/,
-    comment: /\/\/.*/,
-    punctuation: /[{}()\[\]]/,
-    cNull: 'null',
+    whitespace: /[ \t]/,
     NL: { match: /\n/, lineBreaks: true },
-    eof: 'eof'
 })
 
 module.exports = mooLex
